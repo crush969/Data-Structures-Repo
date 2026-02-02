91fb890e089f64ab3b772a7e8996b4fdee7eff63
Austin Norman <anorman3147@floridapoly.edu>
2026-02-02 01:59:23 +0000

    practice 2 coding, logic errors with push_back and print_backwards

--- 03_DLList/DLList.hpp
+++ 03_DLList/DLList.hpp
@@ -10,23 +10,26 @@ class DLList{
         DLList();
 
         // The copy constructor
-        DLList(const DLList<T>& other);
+        //DLList(const DLList<T>& other);
         // Assignment operator
-        DLList<T>& operator=(const DLList<T>& other);
+        //DLList<T>& operator=(const DLList<T>& other);
 
         // The destructor
-        ~DLList();
+        //~DLList();
 
 
-        unsigned    size() const; // Returns the size of the list
+        //unsigned    size() const; // Returns the size of the list
         bool        empty() const; // Returns true if list is empty
         void        push_front(const T& val); // Insert the new node at the beggining
         void        push_back(const T& val); // Insert the new node at the end
         void        print() const; // Print the list
 
+        // Practice 2
+        void        print_backwards() const; // prints the list backwards
+
         void        pop_front(); // Remove the front element (node)
         void        pop_back(); // Remove the last element (node)
-        void        clear(); //Clear the list
+        //void        clear(); //Clear the list
 
     private:
         DLLNode<T>* head; // The pointer to the first node
---
+++ 03_DLList/main_DLList.cpp
@@ -0,0 +1,27 @@
+#include "DLList.hpp"
+#include "DLList.cpp"
+
+#include <iostream>
+
+int main(void) {
+    DLList<char> list;
+
+    list.push_front('a');
+    list.push_front('b');
+    list.push_front('c');
+
+
+    //list.push_back('d');
+    //list.push_back('e');
+    //list.push_back('f');
+
+    list.print();
+
+    list.pop_back();
+
+    list.print();
+    //list.print_backwards();
+
+
+    return 0;
+}
