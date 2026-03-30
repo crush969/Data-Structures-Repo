b42a91865b0de761c2d57180dc181820acd12256
Austin Norman <anorman3147@floridapoly.edu>
2026-03-30 20:51:11 +0000

    update

---
+++ 12_graphs/mainGraph.cpp
@@ -0,0 +1,20 @@
+#include "Graph.hpp"
+
+#include <string>
+
+int main() {
+    Graph<std::string> knowledge;
+
+    knowledge.insertVertex("AVL");
+    knowledge.insertVertex("BST");
+    knowledge.insertVertex("Trie");
+    knowledge.insertVertex("Heap");
+
+    knowledge.insertEdge("BST", "AVL");
+    knowledge.insertEdge("BST", "Trie");
+    knowledge.insertEdge("BST", "Heap");
+
+    knowledge.print();
+
+    return 0;
+}
